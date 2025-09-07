f4606d7ae002dc4753e712b2f32c58f3aa0c4f26
prim1112 <65011212063@msu.ac.th>
2025-09-07 14:57:43 +0700

    update

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { router as index } from "./controller/index";
 
 export const app = express();
-app.use("/", (req, res) => {
-  res.send("Hello World!!!");
-});
+
+app.use("/", index);
+// app.use("/", (req, res) => {
+//   res.send("Hello World!!!");
+// });
