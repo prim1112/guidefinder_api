5709d4f0ee378cccae7d7cfd7252fdd959c82cad
prim1112 <65011212063@msu.ac.th>
2025-10-06 00:28:09 +0700

    update

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import http from "http";
 import { app } from "./app";
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log("Server is started");
+  console.log(`Server running on port ${port}`);
 });
