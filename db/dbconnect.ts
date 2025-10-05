5698fcd9ffeeb3396eb7eb465eeca1100fe481be
prim1112 <65011212063@msu.ac.th>
2025-10-06 00:34:23 +0700

    update

--- db/dbconnect.ts
+++ db/dbconnect.ts
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 const db = mysql.createConnection({
   host: "mysql-guidefinderapp.alwaysdata.net",
-  user: "root",
+  user: "427092",
   password: "65011212063",
   database: "guidefinderapp_db",
 });
