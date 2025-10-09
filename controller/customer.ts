7427e548737c317ec4d58afe7db226337a247c2b
prim1112 <65011212063@msu.ac.th>
2025-10-09 12:48:22 +0700

    update

--- controller/customer.ts
+++ controller/customer.ts
@@ -45,7 +45,7 @@ router.get("/customers", (req: Request, res: Response) => {
   });
 });
 
-router.post("/customers/check-phone", async (req: Request, res: Response) => {
+router.post("/customers_checkphone", async (req: Request, res: Response) => {
   const { phone } = req.body;
 
   const [rows] = await db.execute<RowDataPacket[]>(
