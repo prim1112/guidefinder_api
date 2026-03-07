0f8307d9f32304e9c175f7929f1ae0b0e345b818
pluem46 <65011212155@msu.ac.th>
2026-03-07 14:40:52 +0700

    update

--- dist/db/dbconnect.js
+++ dist/db/dbconnect.js
@@ -7,10 +7,10 @@ const promise_1 = __importDefault(require("mysql2/promise"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const db = promise_1.default.createPool({
-    host: "mysql-final-project.alwaysdata.net",
+    host: "mysql-guidefinderapp.alwaysdata.net",
     user: "guidefinderapp",
     password: "65011212063",
-    database: "final-project_guidefinderapp_db",
+    database: "guidefinderapp_db",
     waitForConnections: true, // ✅ ป้องกัน ECONNRESET
     connectionLimit: 10, // ✅ ใช้ได้ใน Pool
     queueLimit: 0,
