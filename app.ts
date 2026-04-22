a4b902ccd96bdc71f86ee570112b2b0ec5d1c6e6
pluem46 <65011212155@msu.ac.th>
2026-04-22 14:12:25 +0700

    update

--- app.ts
+++ app.ts
@@ -28,6 +28,10 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
 app.use("/", index);
 app.use("/customer", customerRouter);
 app.use("/guide", guideRouter);
@@ -39,7 +43,5 @@ app.use("/booking", bookingRouter);
 //   res.send("Hello World!!!");
 // });
 
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 export default app;
