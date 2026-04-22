a673aa5ed0539adc83ea8754448257a11e433c3e
pluem46 <65011212155@msu.ac.th>
2026-04-22 13:54:57 +0700

    update

--- app.ts
+++ app.ts
@@ -39,4 +39,7 @@ app.use("/booking", bookingRouter);
 //   res.send("Hello World!!!");
 // });
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 export default app;
