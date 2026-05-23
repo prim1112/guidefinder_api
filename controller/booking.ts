ddba07e037369a63c9aeca5dd9c083ec297ee96e
pluem46 <65011212155@msu.ac.th>
2026-05-23 16:43:10 +0700

    update

--- controller/booking.ts
+++ controller/booking.ts
@@ -663,24 +663,48 @@ router.patch("/booking/finish/:bid", async (req: Request, res: Response) => {
   }
 });
 
-router.get("/booking/history/customer/:id", async (req, res) => {
-  const id = req.params.id;
+router.get("/history/customer/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
 
-  const [rows]: any = await db.query(
-    `
-    SELECT *
-    FROM booking_queues
-    WHERE ref_cus_id = ?
-    AND booking_status = 3
-    ORDER BY booking_queue_id DESC
-    `,
-    [id]
-  );
+    const [rows]: any = await db.query(
+      `
+      SELECT 
+        b.booking_queue_id,
+        b.booking_status,
+        b.booking_start_date,
+        b.booking_end_date,
+        b.booking_total_price,
 
-  res.json({
-    message: "ดึงประวัติสำเร็จ",
-    data: rows,
-  });
+        l.travel_name,
+        l.travel_detail,
+        l.travel_image
+
+      FROM booking_queues b
+
+      LEFT JOIN location_travel l
+        ON b.ref_travel_id = l.id
+
+      WHERE b.ref_cus_id = ?
+      AND b.booking_status = 3
+
+      ORDER BY b.booking_queue_id DESC
+      `,
+      [id],
+    );
+
+    return res.status(200).json({
+      message: "ดึงประวัติสำเร็จ",
+      data: rows,
+    });
+  } catch (error: any) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Server Error",
+      error: error.message,
+    });
+  }
 });
 
 export default router;
