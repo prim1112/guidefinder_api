6ea7bce792b4a9c28e1ea8f062cb6768711340c1
pluem46 <65011212155@msu.ac.th>
2026-05-29 15:38:45 +0700

    update

--- services/mail.service.ts
+++ services/mail.service.ts
@@ -1,25 +1,14 @@
-const nodemailer = require("nodemailer");
+import { Resend } from "resend";
+
+const resend = new Resend("re_iqSAYnFG_FBgVjRn3AQ8FDqfroYeanfrM");
 
 export const sendResetEmail = async (email: string, pin: string) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "kawitsara47@gmail.com",
-      pass: "rmbh vnhj psro wupq",
-    },
+  await resend.emails.send({
+    from: "Guide Finder <onboarding@resend.dev>",
+    to: email,
+    subject: "Reset Password PIN",
+    text: `รหัสรีเซ็ตรหัสผ่านของคุณคือ: ${pin}`,
   });
 
-  try {
-    const info = await transporter.sendMail({
-      from: '"Guide Finder" <kawitsara47@gmail.com>',
-      to: email,
-      subject: "Reset Password PIN",
-      text: `รหัสรีเซ็ตรหัสผ่านของคุณคือ: ${pin}`,
-    });
-
-    console.log("EMAIL SENT:", info.messageId);
-  } catch (err) {
-    console.error("EMAIL FAILED:", err);
-    throw err; // 🔥 สำคัญมาก ต้องส่ง error กลับ
-  }
+  console.log("EMAIL SENT");
 };
