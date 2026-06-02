0ec00790c3ae0ec2f2785935d9ffbde6e8d91533
pluem46 <65011212155@msu.ac.th>
2026-06-02 14:48:04 +0700

    update

--- controller/login.ts
+++ controller/login.ts
@@ -395,7 +395,7 @@ router.post("/reset-password", async (req, res) => {
 
 router.get("/test-email", async (req, res) => {
   try {
-    await sendResetEmail("milin04122562@gmail.com", "0412");
+    await sendResetEmail("kawitsara47@gmail.com", "130846");
 
     res.json({
       message: "ส่งเมลสำเร็จ",
